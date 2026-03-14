5cb6d2abb68fbd3352559d54d5d15fe612ec3ace
muzamilsaleem-PostEx <muzamil.saleem@postexglobal.com>
2026-03-14 16:13:36 +0500

    icons

--- src/data/techStack.js
+++ src/data/techStack.js
@@ -1,54 +1,24 @@
 const techStack = [
 
-    {
-        title: "React JS",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-    },
+    { title: "React JS", icon: "/techstack/react.svg" },
 
-    {
-        title: "Next JS",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
-    },
+    { title: "Next JS", icon: "/techstack/nextjs.svg" },
 
-    {
-        title: "Angular",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg"
-    },
+    { title: "Angular", icon: "/techstack/angular.svg" },
 
-    {
-        title: "Node.js",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-    },
+    { title: "Node.js", icon: "/techstack/nodejs.svg" },
 
-    {
-        title: "Express.js",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
-    },
+    { title: "Express.js", icon: "/techstack/express.svg" },
 
-    {
-        title: "MongoDB",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
-    },
+    { title: "MongoDB", icon: "/techstack/mongodb.svg" },
 
-    {
-        title: "MySQL",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-    },
+    { title: "MySQL", icon: "/techstack/mysql.svg" },
 
-    {
-        title: "Git",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
-    },
+    { title: "Git", icon: "/techstack/git.svg" },
 
-    {
-        title: "REST APIs",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-    },
+    { title: "REST APIs", icon: "/techstack/nodejs.svg" },
 
-    {
-        title: "Postman",
-        icon: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
-    }
+    { title: "Postman", icon: "/techstack/postman.svg" }
 
 ];
 
