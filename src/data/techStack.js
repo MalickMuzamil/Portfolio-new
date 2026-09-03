1360bc84ebefc8240342bcba4945628dcda75547
Hafiz Muzamil <malikmuzamil92110@gmail.com>
2026-09-04 00:25:46 +0500

    Updated Stack

--- src/data/techStack.js
+++ src/data/techStack.js
@@ -1,31 +1,23 @@
 const techStack = [
-
     // Data Engineering
     { title: "Python", icon: "/techstack/python.svg" },
-
     { title: "Apache Spark", icon: "/techstack/spark.svg" },
-
     { title: "Databricks", icon: "/techstack/databricks.svg" },
-
     { title: "AWS", icon: "/techstack/aws.svg" },
-
     { title: "Azure", icon: "/techstack/azure.svg" },
-
     { title: "SQL", icon: "/techstack/sql.svg" },
 
     // Full Stack
     { title: "React JS", icon: "/techstack/react.svg" },
-
     { title: "Next JS", icon: "/techstack/nextjs.svg" },
-
+    { title: "Angular", icon: "/techstack/angular.svg" },
     { title: "Node.js", icon: "/techstack/nodejs.svg" },
-
+    { title: "Express.js", icon: "/techstack/express.svg" },
     { title: "MongoDB", icon: "/techstack/mongodb.svg" },
-
     { title: "MySQL", icon: "/techstack/mysql.svg" },
-
     { title: "Git", icon: "/techstack/git.svg" },
-
+    { title: "Postman", icon: "/techstack/postman.svg" },
+    { title: "Tailwind CSS", icon: "/techstack/tailwindcss.svg" },
 ];
 
 export default techStack;
